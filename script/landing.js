22289ef5beda82a7a35a489c6182d3cd359c2767
Sophie Miller <millersg47@gmail.com>
2022-04-04 13:51:44 -0700

    adds to page load function to add image url to image element src, and adds event listener on clicks of the image element

--- script/landing.js
+++ script/landing.js
@@ -1,11 +1,13 @@
-//img element where fetch API will load the APOD on page load event
+//var declared as img element where fetch API will load the APOD on page load event and will be clicked to load modal with image details
 var imageEl = document.querySelector(".image");
+//var declared as the location the user inputs
+var inputEl = document.querySelector(".user-location");
 
 //function to pull the nasa APOD API data to load image and image details on click event
-function getNasaApi() {
+function pageLoad() {
 
     var nasaApiKey = "rjXci6T4vcKLOB8bqde7f6P7zntlo9i8TFoYiiML";
-      // fetch request gets photo and photo details by date
+      // fetch request gets photo and details for current date's pod
       var requestUrl = "https://api.nasa.gov/planetary/apod?api_key=" + nasaApiKey;
     
       fetch(requestUrl)
@@ -13,7 +15,16 @@ function getNasaApi() {
           return response.json();
         })
         .then(function (data) {
-          //logs the array of objects within 'data'
+          //logs the data as an object
           console.log(data)
+          //declares var for url of pod
+          var imageUrl = data.url;
+          //updates the image src attribute with url for pod
+          imageEl.src = imageUrl;
       });
   }
+
+pageLoad();
+
+//event listener for clicks on image element
+imageEl.addEventListener("click", imageClickHandler);
