9427d456219c4bbc90d0d5043c7e463898cd2ff0
Courtney Struelens <courtneystruelens@hotmail.com>
2022-04-04 20:53:34 -0700

    Optimized latitude and longitude function

--- script/main.js
+++ script/main.js
@@ -3,15 +3,13 @@ var key = "&appid=38cb9e992aecb85416eb9cc5841da07c";
 // Returns longitude and latitude from city input
 function getLatLon(city) {
   var url = `https://api.openweathermap.org/geo/1.0/direct?q=${city}${APIKey}`;
-  var cityInfo = { cityName: city, lat: "", lon: "" };
 
   fetch(url)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      // Checks to see if user's city input was valid
-      cityInfo.lat = data[0].lat;
-      cityInfo.lon = data[0].lon;
+      // Assigns city information to an object
+      cityInfo = { cityName: city, lat: data[0].lat, lon: data[0].lon };
     });
 }
